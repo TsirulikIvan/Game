2b4048f126617772b429e7dc4e9223bc1641e8ae
RandiSPB <swat2299@gmail.com>
2019-03-07 04:20:00 +0300

    Первая реализация нормализации карты

--- game1.py
+++ game1.py
@@ -9,13 +9,14 @@ def map_gen(pol_width, pol_height, map_width, map_height, obj = []):
     j = 0
     for i in range(num_line):
         line = []
+        tmp = []
         for j in range(long):
             if (i == 0) or (i == num_line - 1):
                 line.append(1)
             elif (j == 0) or (j == long - 1):
                 line.append(1)
             elif (i > 2) and (j > 2) and (i < num_line - 3) and (j < long - 3):
-                    line.append(random.randint(0, 15))
+                    line.append(random.randint(0, 4))
             else:
                 line.append(0)
                 j += 1
@@ -23,45 +24,41 @@ def map_gen(pol_width, pol_height, map_width, map_height, obj = []):
         i += 1
     return map
 
-
-def map_normal(not_norm_map = []):
-    for i in range(3, len(not_norm_map) - 3):
-        tmp_line = not_norm_map[i]
-        for ind_obj in range(3,len(tmp_line)):
-            cur_fr = []
-            if tmp_line[ind_obj] == 2:
-                print(ind_obj)
-                for j in range(-2, 2):
-                    cur_fr.append(tmp_line[ind_obj + j])
-                print(cur_fr)
-    return not_norm_map
-
 def show_map(Mylist = []):
     for i in Mylist:
         print(i)
 
-#j = нечет -> Стена
+#j = 1 -> Стена
 #j = 2 -> Моб
-#j = 4 -> Дверь горизонт
-#j = 6 -> Дверь вверх
-def draw_map(start_x,start_y,map = [],*col):
+#j = 3 -> Дверь горизонт
+#j = 4 -> Дверь вверх
+def draw_map(start_x, start_y,map = [], *col):
     color = (255, 255, 255)
     x = start_x
     y = start_y
     for i in map:
         for j in i:
-            if (j == 1) or (j == 3) or (j == 5) or (j == 7):
+            if (j == 1):
                 pygame.draw.rect(sc, color, (x, y, width, height))
             elif (j == 2):
                 pygame.draw.rect(sc, (255,0,255), (x, y, width, height))
-
+            elif (j == 0):
+                pygame.draw.rect(sc, (0,0,0), (x, y, width, height))
             x += width
         x = 0
         y += height
 
+def norm_frame(lst = []):
 
+    return 0
 
-
+def form_zone(ind = 3, z_w = 3, z_h = 3, lst =[]):
+    for num_fr in range(ind,len(lst[0]) - 1, h):
+        fr = []
+        for i in range(num_fr,z_h + z_w,1):
+            tmp = lst[i][ind:ind + z_w:1]
+            fr.extend(tmp)
+    return fr
 
 def first_anim(*col):
     pygame.draw.rect(sc, col, (x, y, width, height))
@@ -77,8 +74,34 @@ def lvl():
     pygame.draw.rect(sc, WHITE, (width, height, WIN_WIDTH - 2 * width, WIN_HEIGHT - 2 * height))
     pygame.draw.rect(sc, BLACK, (WIN_WIDTH / 4,  WIN_HEIGHT // 2 - height, WIN_WIDTH /2, height))
 
-def pack(*args):
-    return args
+def normal_map(map = []):
+    flag = 0
+    for i in map:
+        cur_ind = 0
+        if (flag > 2) and (flag < len(map) - 4):
+            while (cur_ind <= len(i) - 1):
+                try:
+                    tmp = i.index(2,cur_ind + 1,len(i) - 1)
+                    cur_ind = tmp
+                    if (cur_ind >= len(i) - 5):
+                        i[cur_ind + 1] = 0
+                        i[cur_ind + 2] = 0
+                    else:
+                        i[cur_ind + 1] = random.randint(0, 1)
+                        i[cur_ind + 2] = random.randint(0, 1)
+                except ValueError:
+                    cur_ind = len(i)
+        if (flag % 3 == 0):
+            print(i)
+            for item in range(len(i)):
+                i[item] = random.randint(0, 1)
+            print(i)
+            flag += 1
+            continue
+        print('')
+        flag += 1
+    return map
+
 FPS = 40
 WIN_WIDTH = 480
 WIN_HEIGHT = 480
@@ -98,23 +121,26 @@ clock = pygame.time.Clock()
 sc = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
 # радиус и координаты круга
-width =  16
-height = 16
+width =  24
+height = 24
 x = 0  # скрываем за левой границей
 y = 0  # выравнивание по центру по вертикали
 startpoint = [WIN_WIDTH - 2 * width, WIN_HEIGHT - 2 * height]
 map = map_gen(width, height, WIN_WIDTH, WIN_HEIGHT)
 
 print(startpoint)
-#show_map(map)
-map_normal(map)
+show_map(map)
+print('')
 draw_map(x, y, map, RED, GREEN, BLUE)
 while True:
 
-
     for i in pygame.event.get():
         if i.type == pygame.QUIT: exit()
-    pygame.display.update()
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_LEFT:
+                map_fin = normal_map(map)
+                draw_map(x, y, map_fin, RED, GREEN, BLUE)
 
+    pygame.display.update()
 
     clock.tick(FPS)
